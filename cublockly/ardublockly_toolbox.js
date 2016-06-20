45635ebd419bad4d65891aa6bfe529b51350a443
Eric Lundby <Eric.Lundby@modrobotics.com>
2016-06-20 16:16:29 -0600

    Remove unsupported toolbox fns.

--- cublockly/ardublockly_toolbox.js
+++ cublockly/ardublockly_toolbox.js
@@ -104,10 +104,10 @@ Cublockly.TOOLBOX_XML =
 '  <category id="catMath" name="Math/Numbers">' +
 '    <block type="math_number"></block>' +
 '    <block type="math_arithmetic"></block>' +
-'    <block type="math_single"></block>' +
-'    <block type="math_trig"></block>' +
-'    <block type="math_constant"></block>' +
-'    <block type="math_number_property"></block>' +
+//'    <block type="math_single"></block>' +
+//'    <block type="math_trig"></block>' +
+//'    <block type="math_constant"></block>' +
+//'    <block type="math_number_property"></block>' +
 '    <block type="math_change">' +
 '      <value name="DELTA">' +
 '        <block type="math_number">' +
