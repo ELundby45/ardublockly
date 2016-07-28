b6ca3812c9f41f7742d0846f94552543d1ee0dec
Eric Lundby <Eric.Lundby@modrobotics.com>
2016-07-28 13:18:39 -0600

    Add in stdbool for now.

--- blockly/generators/cubelets/cubelets.js
+++ blockly/generators/cubelets/cubelets.js
@@ -4,6 +4,7 @@ goog.require('Blockly.Cubelets');
 
 Blockly.Cubelets['cubelets_setup'] = function(block) {
   Blockly.Cubelets.addInclude("cubelet.h", '#include "cubelet.h"')
+  Blockly.Cubelets.addInclude("stdbool.h", '#include <stdbool.h>')
   var setupBranch = Blockly.Cubelets.statementToCode(block, 'setup_do');
   //var setupCode = Blockly.Cubelets.scrub_(block, setupBranch); No comment block
   if (setupBranch) {
