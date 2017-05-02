f80b5615ba360b0822050cb964af4d8aeb91a6d7
serenabux <serena.buxton@colorado.edu>
2017-05-02 15:39:55 -0600

    Includes an error message if the block ID is 0 or less that 0 incase the person accidently forgets to set it when they use the block.

--- blockly/generators/cubelets/messaging.js
+++ blockly/generators/cubelets/messaging.js
@@ -10,6 +10,12 @@ Blockly.Cubelets.set_block_value = function(block) {
  var value_id = Blockly.Cubelets.valueToCode(block, 'ID', Blockly.Cubelets.ORDER_ATOMIC);
  value_block_value = value_block_value || 0;
  value_id = value_id || 0;
+  if(value_id==0|| value_id<0){
+ 	block.setWarningText("Block ID should be greater than zero");
+}
+else{
+		block.setWarningText(null);
+	}
  var code = 'set_block_value(' + value_id + ', ' + value_block_value + ');\n';
  return code;
 };
@@ -17,5 +23,11 @@ Blockly.Cubelets.set_block_value = function(block) {
 Blockly.Cubelets.get_block_value = function(block) {
  var value_id = Blockly.Cubelets.valueToCode(block, 'ID', Blockly.Cubelets.ORDER_ATOMIC);
  value_id = value_id || 0;
+ if(value_id==0|| value_id<0){
+ 	block.setWarningText("Block ID should be greater than zero");
+}
+else{
+		block.setWarningText(null);
+	}
  return ['get_block_value(' + value_id + ')', Blockly.Cubelets.ORDER_ATOMIC]
 };
